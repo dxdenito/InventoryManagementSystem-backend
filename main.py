4ae3e8e9fcf0cb7bb570fee846c9d572bc16bb37
Denis Karanja <denixkaranjah@gmail.com>
2026-04-24 18:09:05 +0300

    cors error fix 7

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 origins=[
-    "http://localhost:5173","http://127.0.0.1:5173",
+    "http://localhost:5173","http://127.0.0.1:5173","null",
 ]
 
 app.add_middleware(
